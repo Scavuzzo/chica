67fdb9b2207e9212d9d1b7652ce06f23a624e685
Lautaro Scavuzzo <lautaro.scavuzzo0@gmail.com>
2023-03-16 22:00:49 -0300

    testing pixel ratio iphone 2

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -20,7 +20,7 @@ export const RAMA_GOTHIC = ramaGothic.style.fontFamily;
 export const BARLOW = barlow.style.fontFamily;
 
 export const globalStyles = css`
-  @media only screen and (-webkit-min-device-pixel-ratio: 2),
+  @media only screen and (-webkit-min-device-pixel-ratio: 3),
   only screen and (min-device-width: 375px) and (max-device-width: 812px) {
     h1 {
       color: blue;
